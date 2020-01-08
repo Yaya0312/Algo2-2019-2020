afa2a200d6c27f02b76662e5ba2db54f1f44e88f
Yaya0312 <Yaya0312@users.noreply.github.com>
2020-01-08 21:34:14 +0100

    Commentaire de doc (réalisé par Baptise)

--- lidx.h
+++ lidx.h
@@ -2,28 +2,46 @@
 #define LIDX__H
 #include "option.h"
 
+//  struct lidx, lidx : structure premettant de stocker différentes 
+//    informations, entre autre, une hashtable contenant les mots du lexique en 
+//    tant que key et une struct ht_val en value, un holdall filenames contenant
+//    tous les noms de fichier représentant un lexique et un holdall words 
+//    contenant tous les strings qui appartiennent au lexique mais qui n'était
+//    pas dans un fichier, enfin, il y a aussi une struct settings qui ??????????????????????????????????????????? 
+//  La structure de données ne stocke pas les objets mais des pointeurs vers ces
+//    objets. 
 typedef struct lidx lidx;
 
-//Création d'un line indexer vide
+//  lidx_empty : créé une structure de données qui contient une hashtable vide,
+//    ainsi que deux holdall vide. Renvoie un pointeur vers l'objet en cas de 
+//    succès, NULL en cas d'échec. Settings sera def ???????????????????????????????
 extern lidx *lidx_empty();
 
-//  Ajoute chaque mots du fichier qui a pour chemin path au line indexer lid
-//    retourne ??
+//  lidx_add_file : ouvre le fichier donné dans path, ajoute le nom du fichier
+//    dans le holdall correspondant se trouvant dans lid, puis ajoute chaques 
+//    mots dans la hashtable de lid. Renvoie 0 en cas de succès et -1 en cas 
+//    d'échec
 extern int lidx_add_file(lidx *lid, char *path);
 
-//Ajoute chaque mots de la chaine de caractéres en tant que lid si il n'est
-//  pas déjà présent
+//  lidx_add_string : ajoute le ou les mots contenus dans dans string dans la
+//    hashtable contenue dans lid. Renvoie 0 en cas de succès et -1 en cas 
+//    d'échec 
 extern int lidx_add_string(lidx *lid, const char **string);
 
-// Mets à jour l'index des lignes à partir du fichier stream
+//  lidx_add_stdin : mets à jour l'index des lignes à partir du fichier stream.
+//    Renvoie 0 en cas de succès et -1 en cas d'échec
 extern int lidx_add_stdin(lidx *lid, FILE *stream);
 
-//Affiche le lidx
+//  lidx_print : vérifie les options si il y en a et les appliques sur les mots,
+//    puis affiche sur la sortie standard les résultats attendus par l'utilisa-
+//    teur   
 extern void lidx_print(lidx *lid);
 
-//Definie les options d'affichage du line indexer lid avec les options opts
+//  lidx_set_options : définie les options d'affichage pour les mots contenus 
+//    dans la hashtable de lid avec les options opts
 extern void lidx_set_options(lidx *lid, settings *opts);
 
-//Nettoyage du lidx lidx
+//  lidx_dispose : libère les ressources allouées à la structure de données 
+//    associée à *lid puis affecte à *lid la valeur NULL
 extern void lidx_dispose(lidx *lid);
 #endif
